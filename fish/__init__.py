710a2d76a0f5367a33b2ebceb63debbc9e5bb83e
Knarkzel <knarkzel@gmail.com>
2022-08-02 00:02:00 +0200

    Use absolute paths

--- fish/__init__.py
+++ fish/__init__.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask, render_template, request, redirect, flash, session
 import base64
+import pathlib
 
-
+root_folder = pathlib.Path(__file__).parent.resolve()
+image_folder = os.path.join(root_folder, "static/images")
 
 app = Flask(__name__)
-app.secret_key = "good-secret_key"
+app.secret_key = "821da87006d0296aea3c0f091a88c097d4709493f7a972566b444e33c3a948bb"
 
 users = []
 class User:
@@ -16,17 +18,11 @@ class User:
     def __eq__(self, other):
         return self.username == other.username
 
-
-
-
-
-
-
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         user = User(username, password)
         if user in users:
             return user.id
@@ -37,9 +33,8 @@ def register():
             return redirect("/")
     else:
         return render_template("register.html")
-        
     
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
@@ -48,8 +43,8 @@ def logout():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form["username"]
+        password = request.form["password"]
         login = User(username, password)
         for user in users:
             if user.id == login.id:
@@ -60,11 +55,10 @@ def login():
     else:
         return render_template("login.html")
 
-@app.route('/')
+@app.route("/")
 def index():
-    
     images = []
-    for file in os.listdir("./fish/static/images"):
+    for file in os.listdir(image_folder):
             if file != ".gitkeep":
                     images.append("/static/images/" + file)
     return render_template("index.html", images=images)
@@ -73,11 +67,11 @@ def index():
 def upload_file():
     if request.method == "POST":
         file = request.files["file"]
-        path = os.path.join("./fish/static/images", file.filename)
+        path = os.path.join(image_folder, file.filename)
         file.save(path)
         return redirect("/")
     else:
-        return render_template('upload.html')
+        return render_template("upload.html")
 
 
 
