5de6a7283fa6447742f247cc77ed0797925a5960
Knarkzel <knarkzel@gmail.com>
2022-08-01 20:22:01 +0200

    sleeeek

--- fish/__init__.py
+++ fish/__init__.py
@@ -1,22 +1,12 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
         return render_template("index.html")
 
-@app.route('/about')
-def about():
-        return render_template("about.html")
-
-@app.route('/map')
-def map():
-        return render_template("map.html")
-
-@app.route('/comments/')
+@app.route('/comments')
 def comments():
     comments = ['This is the first comment.',
                 'This is the second comment.',
