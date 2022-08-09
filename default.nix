df014a77996d289f5ab481a95af3f4f2268639af
Knarkzel <knarkzel@gmail.com>
2022-08-09 19:24:41 +0200

    wip: add georust to default

--- default.nix
+++ default.nix
@@ -1,6 +1,10 @@
 {python3Packages ? (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e3583ad6e533a9d8dd78f90bfa93812d390ea187.tar.gz") {}).python3Packages}:
-with python3Packages;
-  buildPythonPackage {
+let
+  georust = fetchTarball {
+    url = "https://git.sr.ht/~knarkzel/georust/archive/f58f21de85d6e391ed864f48fb655e9aabcf0bd1.tar.gz";
+    sha256 = "1cyypr315yf15ilxdr3l069lgf0ip92m15rg2djczbymln38y1ar";
+  };
+in with python3Packages; buildPythonPackage {
     name = "fish";
     src = ./src;
 
@@ -12,7 +16,8 @@ with python3Packages;
 pgrep python | xargs kill -9 2>/dev/null
 python -m flask run"
     '';
-    propagatedBuildInputs = [flask exif folium pillow sqlitedict requests timeago];
+
+    propagatedBuildInputs = [georust flask exif folium pillow sqlitedict requests timeago];
 
     installPhase = ''
       runHook preInstall
