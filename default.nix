ad43492e66fe2dfe1e8a90d00239704594882fc2
Knarkzel <knarkzel@gmail.com>
2022-08-09 22:45:29 +0200

    Fix georust issue

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 {python3Packages ? (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e3583ad6e533a9d8dd78f90bfa93812d390ea187.tar.gz") {}).python3Packages}:
 let
-  georust = fetchTarball {
+  georust = import (fetchTarball {
     url = "https://git.sr.ht/~knarkzel/georust/archive/f58f21de85d6e391ed864f48fb655e9aabcf0bd1.tar.gz";
     sha256 = "1cyypr315yf15ilxdr3l069lgf0ip92m15rg2djczbymln38y1ar";
-  };
+  });
 in with python3Packages; buildPythonPackage {
     name = "fish";
     src = ./src;
@@ -17,7 +17,8 @@ pgrep python | xargs kill -9 2>/dev/null
 python -m flask run"
     '';
 
-    propagatedBuildInputs = [georust flask exif folium pillow sqlitedict requests timeago];
+    nativeBuildInputs = [georust];
+    propagatedBuildInputs = [flask exif folium pillow sqlitedict requests timeago];
 
     installPhase = ''
       runHook preInstall
